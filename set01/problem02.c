9713cea6c51409ccbd76836b64468b41e57bf3e0
penmetsasharitha <penmetsas.bsc23@rvu.edu.in>
2024-01-23 10:04:41 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -2,8 +2,15 @@
 #include <stdio.h>
 int main()
 {
-    int num1,num2;
+    int num1,num2,sum;
 
     printf("enter the frist numder:");
-    scanf("%d")
+    scanf("%d",& num1);
+
+    printf("enter the second numder:");
+    scnaf("%d",& num2);
+
+    sum =num1+num2;
+    printf("sum of %d and %d",num1,num2,sum);
+    return 0;
 }
